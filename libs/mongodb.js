13289b7950d1df307fed2acef291bca36b03f23e
okanajonathan2@gmail.com <okanajonathan2@gmail.com>
2026-02-02 16:43:09 +0100

    connect to database

---
+++ libs/mongodb.js
@@ -0,0 +1,22 @@
+// mongodb.js
+import mongoose from "mongoose";
+
+let isConnected = false;
+
+const connect = async () => {
+    if (isConnected) return;
+    
+    try {
+        const conn = await mongoose.connect(process.env.MONGO_URI, {
+        dbName: process.env.DB_NAME,
+        });
+
+        isConnected = conn.connections[0].readyState === 1;
+        console.log("Mongo connected ! 🫨 ");
+    } catch (e) {
+        console.error("Erreur de connexion MongoDB : ", e.message);
+        throw new Error("Error in connecting");e
+    }
+};
+
+export default connect;
